0137dc6ba5f3f9abb2342c9bba81e5206ce9a440
|erdogan| <94300378+erdogancayir@users.noreply.github.com>
2022-12-19 22:32:20 +0300

    Update class_value_check.py

--- class_value_check.py
+++ class_value_check.py
@@ -11,3 +11,19 @@ print(ex_object.a)
 #class icinde bu deger varmi diye kontrol eder varsa if e girer.
 if (hasattr(ex_object, 'b')):
     print(ex_object.b)
+ 
+
+#---------------------------------
+
+class ExampleClass:
+    a = 1
+    def __init__(self):
+        self.b = 2
+
+
+example_object = ExampleClass()
+
+print(hasattr(example_object, 'b'))
+print(hasattr(example_object, 'a'))
+print(hasattr(ExampleClass, 'b'))
+print(hasattr(ExampleClass, 'a'))
