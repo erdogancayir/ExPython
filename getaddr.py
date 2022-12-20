c3ac276fb01ccca45b29cf0e41cc8699bbf5e83e
|erdogan| <94300378+erdogancayir@users.noreply.github.com>
2022-12-20 17:31:57 +0300

    Update getaddr.py

--- getaddr.py
+++ getaddr.py
@@ -7,3 +7,15 @@ person = Student()
 name = getattr(person, 'name')
 
 print(name)
+
+#------------------------------------------
+class Student:
+  marks = 88
+  name = 'Sheeran'
+
+person = Student()
+
+# set value of name to Adam
+setattr(person, 'name', 'Adam')
+
+print(person.name)
