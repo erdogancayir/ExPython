1ea10a4f946d6c4632ca645f0da575495ed3104e
|erdogan| <94300378+erdogancayir@users.noreply.github.com>
2023-01-30 12:59:48 +0300

    Create system_command.py

---
+++ system_command.py
@@ -0,0 +1,23 @@
+import os
+
+class DirectorySearcher:
+    def find(self, path, dir):
+        try:
+            os.chdir(path)
+        except OSError:
+            # Doesn't process a file that isn't a directory.
+            return
+
+        current_dir = os.getcwd()
+        for entry in os.listdir("."):
+            if entry == dir:
+                print(os.getcwd() + "/" + dir)
+            self.find(current_dir + "/" + entry, dir)
+
+
+directory_searcher = DirectorySearcher()
+directory_searcher.find("./tree", "python")
+#path="./tree", dir="python"
+#.../tree/python
+#.../tree/cpp/other_courses/python
+#.../tree/c/other_courses/python
